cdc051fdec982d732588677c3bf8c6c7910a039a
Luis Bustos <luisbustos500@gmail.com>
2022-10-21 15:00:53 -0600

    objects

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 /*
+/////////////////////////////////////////////////
 // STRINGS 
+/////////////////////////////////////////////////
 let firstVariable = "Hello World";
 firstVariable = 25; 
 
@@ -13,7 +15,9 @@ console.log(greeting);
 
 
 
+/////////////////////////////////////////////////
 //BOOLEANS
+/////////////////////////////////////////////////
 const a = 4;
   const b = 53;
   const c = 57;
@@ -34,7 +38,9 @@ const a = 4;
 
 
 
+/////////////////////////////////////////////////
 //the Farm
+/////////////////////////////////////////////////
 let animal = "pig";
 if (animal === "cow") {
     console.log('moooo');
@@ -44,7 +50,9 @@ if (animal === "cow") {
 
 
 
+/////////////////////////////////////////////////
 //Driver's Ed
+/////////////////////////////////////////////////
 let personsAge = 12;
 if (personsAge >= 16){
     console.log('Here are the keys!');
@@ -54,7 +62,9 @@ if (personsAge >= 16){
 
 
 
+/////////////////////////////////////////////////
 //LOOPS
+/////////////////////////////////////////////////
 //A - The Basics
 for(let i = 0; i <= 10; i++) {
     console.log(i);
@@ -76,7 +86,9 @@ console.log(loop3);
 
 
 
+/////////////////////////////////////////////////
 //B - Get Even 
+/////////////////////////////////////////////////
 for(let i = 1; i <= 100; i++){
     if ( i % 2 == 0){
        console.log( i + ' <-- is a even number '); 
@@ -87,7 +99,9 @@ for(let i = 1; i <= 100; i++){
 
 
 
+/////////////////////////////////////////////////
 //Give Me 5
+/////////////////////////////////////////////////
 for(let i = 1; i <= 100; i++){
     if((i % 3 == 0) && (i % 5 == 0)) {
         console.log('I found a ' + i + ". High five!" + ' Three is a crowd')
@@ -102,7 +116,9 @@ for(let i = 1; i <= 100; i++){
 
 
 
+/////////////////////////////////////////////////
 //Savings Account
+/////////////////////////////////////////////////
 let bank_account = 0;
 for(let i = 1; i <= 100; i++){
     bank_account += i * 2;
@@ -111,14 +127,17 @@ console.log(bank_account);
 
 
 
-
+////////////////////////////////////////////////////////////////////
 //ARRAYS & CONTROL FLOW
+///////////////////////////////////////////////////////////////////
 //Easy Does It
 const quotes = ["quote 1", "quote 2", "quote 3"];
 
 
 
+/////////////////////////////////////////////////
 //Accessing elements
+/////////////////////////////////////////////////
 const randomThings = [1, 10, "Hello", true]
 //1 
 randomThings[0];
@@ -129,7 +148,9 @@ console.log(randomThings);
 
 
 
+/////////////////////////////////////////////////
 //Change values
+/////////////////////////////////////////////////
 const ourClass = ["Salty", "Zoom", "Sardine", "Slack", "Github"]
 //1
 ourClass[2];
@@ -141,7 +162,9 @@ console.log(ourClass);
 
 
 
+/////////////////////////////////////////////////
 //Mix It Up
+/////////////////////////////////////////////////
 const myArray = [5, 10, 500, 20]
 //1
 myArray.push('Aegon', 'String');
@@ -157,6 +180,199 @@ console.log(myArray);
 //Mutate is the act of changing the properties of a value.
 //Just the order of the array was changed.
 
-*/
 
+
+/////////////////////////////////////////////////
 //Biggie Smalls
+/////////////////////////////////////////////////
+let num = 253;
+if(num < 100) {
+    console.log("little number");
+} else {
+    console.log('big number')
+};
+
+
+
+//////////////////////////////////////////////////
+//Monkey in the Middle
+//////////////////////////////////////////////////
+let mm = 7;
+if (mm < 5) {
+    console.log('little number');
+} else if(mm > 10) {
+    console.log('big number');
+}else {
+    console.log('monkey');
+};
+
+
+
+///////////////////////////////////////////////////
+//Whats in your closet
+///////////////////////////////////////////////////
+const kristynsCloset = [
+    "left shoe",
+    "cowboy boots",
+    "right sock",
+    "GA hoodie",
+    "green pants",
+    "yellow knit hat",
+    "marshmallow peeps"
+  ];
+
+console.log('Kristyn is rocking that ' + kristynsCloset[2] + ' today!');
+kristynsCloset.splice(6, 0, 'raybans');
+kristynsCloset[5] = 'Stained knit hat';
+console.log(kristynsCloset);
+
+//Toms Closet//
+const thomsCloset = [
+    [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+    ],[
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+    ],[
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+    ]
+  ];
+
+thomsCloset[1][2] = 'Footie Pajamas';
+let thomsShirt = thomsCloset[0][0];
+let thomsPants = thomsCloset[1][2];
+let thomsAccessories = thomsCloset[2][2];
+console.log('Thom is looking fierce in a ' + thomsShirt + ', ' + thomsPants + ' and ' + thomsAccessories + '!');
+
+
+
+/////////////////////////////////////////////////////////////////
+//Functions
+/////////////////////////////////////////////////////////////////
+
+//B
+function printCool(name) {
+    console.log(name + ' is cool.');
+}
+printCool('Luis');
+
+//C
+const calculateCube = (num) => { return num * num * num };
+console.log(calculateCube(5));
+
+//D
+const isVowel = (v) => {
+    v = v.toLowerCase();
+    if (v === 'a' || v === 'e' || v === 'i' || v === 'o' || v === 'u'){
+        return true;
+    } else {
+        return false;
+    }
+}
+console.log(isVowel('o'));
+console.log(isVowel('f'));
+console.log(isVowel('A'));
+
+//E
+const getTwoLengths = (p1, p2) => {
+    return [p1.length, p2.length]
+}
+console.log(getTwoLengths('yo', 'Hippopopalous'));
+
+
+//F               
+const getMultipleLengths = ([...args]) => {
+    for(let i = 0; i < args.length; i++){
+        console.log(args[i].length);
+    }
+}
+getMultipleLengths(['yo', 'avatar', 'windows', 'ice']);
+
+//G
+const maxOfThree = (n1, n2, n3) => {
+    if (n1 >= n2 && n1 >= n3) {
+        return n1
+    } if(n2 >= n1 && n2 >= n3) {
+        return n2
+    } else {
+        return n3
+    }
+};
+console.log(maxOfThree(8, 8, 3));
+
+//H           //IDK//
+function printLongestWord(long) {
+    let longestWord = 0;
+    for(let i = 0; i < long.length; i++) {
+        if(long[i].length > longestWord){
+            longestWord = long[i].length;
+        }
+    }
+    console.log(longestWord);
+};
+printLongestWord(["BoJack", "Princess", "Diane", "a", "Max", "Peanutbutter", "big", "Todd"]);
+
+
+
+//////////////////////////////////////////////////////
+//Objects
+//////////////////////////////////////////////////////
+//A
+let user = {
+    name: 'billy',
+    email: 'billy@email.com',
+    age: 32,
+    purchased: [],
+    friend: {
+        name: 'ed',
+        age: '45',
+        location: 'india',
+        purchased: [],
+    }
+};
+//B
+user.email = 'legend27@email.com'
+user.age++
+//C
+user.location = 'poland';
+//D
+user.purchased.push('carbohydrates', 'peace of mind', 'Merino jodhpurs');
+console.log(user.purchased[2]);
+//E
+console.log(user.friend.name);
+console.log(user.friend.location);
+user.friend.age = 55;
+user.friend.purchased.push('The One Ring', 'A latte');
+console.log(user.friend.purchased[1]);
+//F
+for(let i = 0; i < user.purchased.length; i++) {
+    console.log(user.purchased[i]);
+};
+
+for(let i = 0; i < user.friend.purchased.length; i++) {
+    console.log(user.friend.purchased[i]);
+};
+//G
+// const updateUser = () => {
+//     user.age++,
+//     user.name = user.name.toUpperCase();
+// };
+// updateUser();
+console.log(user);
+
+function oldAndLoud(person) {
+    person.age++,
+    person.name = person.name.toUpperCase();
+};
+oldAndLoud(user);
+
+*/
